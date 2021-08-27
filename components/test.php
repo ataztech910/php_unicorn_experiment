b677360d348f3da97e2af4706f2e7e3b15254d06
ataztech910 <webconsult.ekb@gmail.com>
2021-08-27 21:32:15 +0200

    remove unnecessary comments

--- components/test.php
+++ components/test.php
@@ -1,7 +1,5 @@
 <?php
-// $value = $_GET["test_request_value"];
 function TestComponent($testInternal = "") {
-    // global $value;
     $testVariable = "test".$testInternal;
     ?>
         <div class="TestComponent" id="TestComponent"><?php echo $testVariable; ?></div>
